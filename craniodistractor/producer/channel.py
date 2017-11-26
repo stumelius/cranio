49800cf13717f9baecb5e35bbaafdeea031bec02
Simo Tumelius <simo.tumelius@gmail.com>
2017-11-26 11:52:25 +0200

    added copyright notices

--- craniodistractor/producer/channel.py
+++ craniodistractor/producer/channel.py
@@ -1,4 +1,21 @@
-''' Producer channel implementation '''
+'''
+MODULE DESCRIPTION
+
+Copyright (C) 2017  Simo Tumelius
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 # IMADA HTG2-4 digital force gauge
 # SN: FTSLQ6QIA
 import serial.tools.list_ports
