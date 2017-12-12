adc5ec4ef50870af3dd5fdfae25831407f02671c
Simo Tumelius <simo.tumelius@gmail.com>
2017-12-12 18:57:50 +0200

    issue #5 removed twisted from install_requires and removed an echo server script from console_scripts

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup
 setup(
     name='craniodistractor',
     packages=['craniodistractor'],
-    install_requires=['twisted', 'pyqtgraph', 'pandas'],
+    install_requires=['pandas', 'pyqtgraph'],
     entry_points={
-        'console_scripts': [
-            'echo_server = craniodistractor.server.echo_server:run_server'
-        ],
+        'console_scripts': [],
     },
 )
