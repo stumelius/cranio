f9de5266c0b9b261effd0c435eabb68a74c5296b
Simo Tumelius <simo.tumelius@gmail.com>
2018-01-11 18:48:33 +0200

    Add pluggable imada and dummy sensor components to the run script

--- run.py
+++ run.py
@@ -14,6 +14,19 @@ def update(plot_widget):
 
 def random_value_generator():
     return random.gauss(0, 1)
+
+def plug_imada_sensor(producer_process):
+    s = ImadaSensor()
+    producer_process.producer.add_sensor(s)
+    return s
+
+def plug_dummy_sensor(producer_process):
+    s = Sensor()
+    s._default_value_generator = random_value_generator
+    ch = ChannelInfo('torque', 'Nm')
+    s.add_channel(ch)
+    producer_process.producer.add_sensor(s)
+    return s
     
 def run():
     ''' Runs the cranio prototype '''
@@ -22,18 +35,15 @@ def run():
     w = PlotWidget()
     p.add_plot(w)
     w.producer_process = ProducerProcess('Imada torque producer')
-    #s = ImadaSensor()
+    # add imada sensor
+    #s = plug_imada_sensor(w.producer_process)
     
     # add dummy sensor with a torque channel
-    s = Sensor()
-    s._default_value_generator = random_value_generator
-    ch = ChannelInfo('torque', 'Nm')
-    s.add_channel(ch)
+    s = plug_dummy_sensor(w.producer_process)
     
     # set axis labels
     w.x_label = 'time (s)'
-    w.y_label = str(ch)
-    w.producer_process.producer.add_sensor(s)
+    w.y_label = str(s.channels[0])
     return p.exec_()
 
 
