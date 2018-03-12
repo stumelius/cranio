73597130b5a0ba237eb7db411d579020105ddcf8
Simo Tumelius <simo.tumelius@gmail.com>
2018-03-12 17:39:27 +0200

    Add pip cache to travis configuration (#31)

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='cranio',
     packages=['cranio', 'cranio.app'],
     install_requires=['pandas', 'pyserial', 'attrs', 'pyqt5', 'pyqtgraph'],
+    extras_require={
+        'dev': [],
+        'test': ['pytest'],
+    },
     entry_points={
         'console_scripts': [],
     },
