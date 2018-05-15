535229be1dd229be6a10988abe1b8e30f66ff453
Unknown <simo.tumelius@gmail.com>
2018-05-15 20:15:36 +0300

    Add FileObject, Index and FileDatabase objects (#43)
    * Add unit test module for database-related objects

--- cranio/document.py
+++ cranio/document.py
@@ -1,6 +1,72 @@
 '''
 DUMMY MODULE
 '''
+from pathlib import Path
+from typing import Union, List
 DOCUMENT_NAME_TEMPLATE = '{}.json'
 DATA_NAME_TEMPLATE = '{}.csv'
 LOG_NAME_TEMPLATE = '{}.txt'
+
+ENCODING = 'utf-8'
+
+
+class FileObject:
+    content_type = 'text/plain'
+
+    def __init__(self, path= Union[str, Path]):
+        self.path = path
+        self.content = None
+
+    def read(self, path: Union[str, Path]) -> str:
+        ''' Read from file '''
+        with open(str(path), 'r', encoding=ENCODING) as f:
+            self.content = f.read()
+        return self.content
+
+    def write(self, path: Union[str, Path]) -> None:
+        ''' Write to file '''
+        self.path = path
+        with open(str(self.path), 'w', encoding=ENCODING) as f:
+            f.write(self.content)
+
+
+class Index(FileObject):
+
+    def __init__(self, path: Union[str, Path]):
+        super().__init__()
+        self.path = path
+        self.file_objects = []
+
+    def load(self):
+        ''' Load index from disk '''
+        self.file_objects = self.parse(self.read(self.path))
+        return self
+
+    @classmethod
+    def parse(cls, index_str: str) -> List[FileObject]:
+        ''' Index string format: "content_type: path_to_file" '''
+        file_objects = []
+        for line in index_str.splitlines():
+            content_type, path = (x.strip() for x in line.split(':'))
+            file_object = FileObject(path)
+            file_object.content_type = content_type
+            file_objects.append(file_object)
+        return file_objects
+
+
+class FileDatabase:
+    ''' Simple file database '''
+    index_name = 'index'
+
+    def __init__(self, index_path: Union[str, Path]):
+        self.index_path = index_path
+        self.index = None
+
+    def commit(self):
+        ''' Commit all changes to disk '''
+        raise NotImplementedError
+
+    def load(self):
+        ''' Load database from disk '''
+        self.index = Index(self.index_path).load()
+        return self
---
+++ test/test_database.py
@@ -0,0 +1,76 @@
+import pytest
+import os
+import copy
+from typing import Union
+from contextlib import suppress
+from cranio.document import FileDatabase, FileObject, Index, ENCODING
+from pathlib import Path
+
+
+def try_remove(name: Union[str, Path]):
+    ''' Try to remove a file from the file system '''
+    if name is not None:
+        with suppress(PermissionError, FileNotFoundError):
+            os.remove(str(name))
+
+
+@pytest.fixture
+def txt_path():
+    content = 'foo bar'
+    path = 'foo.txt'
+    with open(path, 'w', encoding=ENCODING) as f:
+        f.write(content)
+    yield path
+    try_remove(path)
+
+
+@pytest.fixture
+def csv_path():
+    content = 'foo;bar'
+    path = 'foo.csv'
+    with open(path, 'w', encoding=ENCODING) as f:
+        f.write(content)
+    yield path
+    try_remove(path)
+
+
+@pytest.fixture
+def index_path(txt_path, csv_path):
+    content = '\n'.join(['text/plain: ' + txt_path,
+                         'text/csv: ' + csv_path])
+    path = 'index'
+    with open(path, 'w', encoding=ENCODING) as f:
+        f.write(content)
+    yield path
+    try_remove(path)
+
+
+def test_FileObject_read_and_write(txt_path):
+    path = txt_path
+    f = FileObject()
+    assert f.content is None
+    f.read(path)
+    assert f.content is not None
+    content = copy.copy(f.content)
+    name = 'foo.txt'
+    # write to file
+    f.write(name)
+    # read and verify content
+    assert f.read(name) == content
+    try_remove(name)
+
+def assert_default_index(index):
+    assert len(index.file_objects) == 2
+    assert index.file_objects[0].content_type == 'text/plain'
+    assert index.file_objects[1].content_type == 'text/csv'
+
+def test_Index_load(index_path):
+    index = Index(index_path).load()
+    assert_default_index(index)
+
+
+def test_FileDatabase_load(index_path):
+    db = FileDatabase(index_path).load()
+    assert_default_index(db.index)
+
+
